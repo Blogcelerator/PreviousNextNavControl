2b0c4513cda4e39e4ad1a3288a10ffee22c6c73a
Joshua Jones <the@joshbjones.com>
2014-03-02 21:16:44 -0600

    Removed Registration Hooks
    -Tested File
    -Removed Registration Plugin
    -Commented Out Is Single Test On Functions

--- PreviousNextNavControl.php
+++ PreviousNextNavControl.php
@@ -2,7 +2,7 @@
    /*
    Plugin Name: PreviousNextNavControl
    Plugin URI: http://blogcelerator.com
-   Description: a plugin that controls 
+   Description: a plugin that controls Previous and Next Page links at the bottom of posts
    Version: 1.0
    Author: Joshua Jones
    Author URI: http://joshbjones.com
@@ -11,30 +11,34 @@
    
    /*
    Runs during plugin activation
-   */
-   register_activation_hook(__FILE__,'PreviousNextNavControl.php');
+   register_activation_hook(__FILE__,'PreviousNext_Install');
    
-   function Sort_Previous_Nav(){
-	if(is_single()){
-		global $post;
-		$prev_posts = previous_post_link('<span class="prev">%link</span>', '<em class="arrow">&laquo;</em> %title', $in_same_cat, '353');
-		if
+   function PreviousNext_Install(){
+   
+   }
+      */
+	  
+      /* 
+   Runs during plugin deactivation
+
+	register_deactivation_hook( __FILE__, 'PreviousNext_Uninstall' );
+	
+	function PreviousNext_Uninstall(){
+	
 	}
+	   */
+   function Sort_Previous_Nav(){
+	/*if(is_single()){*/
+		$prev_posts = previous_post_link('<span class="prev">%link</span>', '<em class="arrow">&laquo;</em> %title', $in_same_cat, '353');
+	/*}*/
    }
    
    function Sort_Next_Nav(){
-	if(is_single()){
-		global $post;
+	/*if(is_single()){*/
 		$next_posts = next_post_link('<span class="next">%link</span>', '<em class="arrow">&raquo;</em> %title', $in_same_cat, '353');
-		if
-	}
+	/*}*/
    }
    
    add_action("previous_post_link", "Sort_Previous_Nav");   
    add_action("next_post_link", "Sort_Next_Nav");
-
-   /* 
-   Runs during plugin deactivation
-   */
-	register_deactivation_hook( __FILE__, 'PreviousNextNaveControl.php' );
 ?>
---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== PreviousNextNavControl ===
+Contributors: thejoshbjones
+Requires at least: 3.8
+Tested up to: 3.8
+
+A Plugin used to control Page Navigation at the bottom of single posts.
+
+== Description ==
+This is a plugin that will allow you to select a specific category to exclude from posts
